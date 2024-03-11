146349c0410fe1c8a61867055fe9c5090862fafb
Nikita Safonov <148264012+nxplosive@users.noreply.github.com>
2024-03-11 04:51:57 +0300

    Amen

---
+++ tests/api/test_location_api.py
@@ -0,0 +1,27 @@
+import json
+
+import allure
+import requests
+from allure_commons._allure import step
+from jsonschema import validate
+
+from lamoda_tests.utils.schemas_path import load_schema
+
+
+@allure.tag('api')
+@allure.label('layer', 'API')
+@allure.label('owner', 'nsafonov')
+@allure.story('Выбор локации')
+def test_location(base_url):
+    with step("Выбор локации"):
+        url = f"{base_url}/delivery/methods?city_aoid=7700000000000&ignore_cart=true"
+    headers = {"Content-Type": "application/json"}
+    response = requests.request("GET", url, headers=headers)
+    body = response.json()
+    with allure.step('Статус код = 200'):
+        assert response.status_code == 200
+
+    schema = load_schema("geo.json")
+    with open(schema) as file:
+        schema = json.load(file)
+    validate(body, schema=schema)
