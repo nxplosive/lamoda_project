4c0cfcb7b40e17994c114ca253ca27235c667e18
Nikita Safonov <148264012+nxplosive@users.noreply.github.com>
2024-03-11 12:07:35 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     window_width: str = '1920'
     window_height: str = '1080'
     base_url: str = 'https://www.lamoda.ru'
-    remote: bool = False
+    remote: bool = True
     page_load_strategy: str = 'normal'
     selenoid_capabilities: dict = {
         "browserName": "chrome",
